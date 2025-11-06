8da19ce361417791ac5720b381f44af9250287c2
Krzysztof <krzysztof@MSI.Bravo>
2025-11-06 22:22:06 +0100

    feat: integrate GameState autoload and working ECTS collection system

--- scripts/items/ects_coin.gd
+++ scripts/items/ects_coin.gd
@@ -1,11 +1,13 @@
-extends Node
+extends Area2D
 
+@export var ects_value: int = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
 
+func _on_body_entered(body: Node) -> void:
+	if not body.is_in_group("player"):
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	GameState.add_ects(ects_value)
+	queue_free()
--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -2,37 +2,34 @@ extends CanvasLayer
 
 @onready var hearts_container = $HeartsContainer
 @onready var energy_container = $EnergyContainer
-@onready var ects_label = $EctsContainer/EctsLabel # Renamed from CoinLabel
-
-var game_state = null # We will store the reference here
+@onready var ects_label = $CoinContainer/CoinLabel
 
 func _ready():
-	# Wait for the Main scene to be ready and find GameState
-	await get_tree().root.ready 
-	game_state = get_node("/root/Main/GameState")
-	
-	if game_state:
-		# Connect to signals from our GameState
-		game_state.lives_changed.connect(update_lives)
-		game_state.energy_changed.connect(update_energy)
-		game_state.ects_changed.connect(update_ects)
-		
-		# Set initial values
-		update_lives(game_state.lives)
-		update_energy(game_state.energy)
-		update_ects(game_state.ects)
-	else:
-		print("CRITICAL ERROR: HUD.gd could not find /root/Main/GameState")
+	# Połączenie HUD z globalnym GameState (autoload)
+	GameState.lives_changed.connect(update_lives)
+	GameState.energy_changed.connect(update_energy)
+	GameState.ects_changed.connect(update_ects)
 
+	# Ustawienie wartości początkowych
+	update_lives(GameState.lives)
+	update_energy(GameState.energy)
+	update_ects(GameState.ects)
+
+
+# --- AKTUALIZACJA ŻYCIA ---
 func update_lives(new_lives):
 	var hearts = hearts_container.get_children()
 	for i in range(hearts.size()):
-		hearts[i].visible = (i < new_lives) 
+		hearts[i].visible = (i < new_lives)
 
+
+# --- AKTUALIZACJA ENERGII ---
 func update_energy(new_energy):
 	var energy_drinks = energy_container.get_children()
 	for i in range(energy_drinks.size()):
 		energy_drinks[i].visible = (i < new_energy)
 
+
+# --- AKTUALIZACJA PUNKTÓW ECTS ---
 func update_ects(new_ects):
-	ects_label.text = "x" + str(new_ects)
+	ects_label.text = str(new_ects) + "/30"
