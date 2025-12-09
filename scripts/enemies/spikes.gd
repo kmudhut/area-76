3a7815c5e2f63158fafb485af2739ca37ea70470
kmudhut <kamil.lepianka.03@gmail.com>
2025-12-09 14:53:31 +0100

    Added spikes enemy.

---
+++ scripts/enemies/spikes.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@onready var sprite = $AnimatedSprite2D
+@onready var attack_area = $AnimatedSprite2D/Area2D
+@onready var timer = $Timer
+
+@export var time_to_stay_out: float = 1
+@export var time_to_stay_in: float = 2.0 
+
+var spikes_out: bool = false
+var state_timer: Timer
+
+func _ready() -> void:
+	state_timer = Timer.new()
+	state_timer.one_shot = true
+	state_timer.timeout.connect(_on_timer_timeout)
+	add_child(state_timer)
+	to_idle_mode()
+
+func _on_timer_timeout() -> void:
+	if spikes_out:
+		to_idle_mode()
+	else:
+		to_attack_mode()
+
+func to_attack_mode() -> void:
+	spikes_out = true
+	sprite.play("show_spikes")
+	attack_area.monitoring = true
+	
+	state_timer.wait_time = time_to_stay_out
+	state_timer.start()
+
+func to_idle_mode() -> void:
+	spikes_out = false
+	sprite.play("hide_spikes")
+	attack_area.monitoring = false
+	
+	state_timer.wait_time = time_to_stay_in
+	state_timer.start()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("player")):
+		body.take_damage(10)
