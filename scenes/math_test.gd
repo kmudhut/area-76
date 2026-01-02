74253c5cdb0db5c335fa6c1dc0e6017438ed67e3
Kamil <kamil.lepianka.03@gmail.com>
2026-01-02 02:14:24 +0100

    Added timer to MathTest node.

--- scenes/math_test.gd
+++ scenes/math_test.gd
@@ -1,6 +1,10 @@
 extends CenterContainer
 @onready var exercise_texture_rect = $Panel/VBoxContainer/MarginContainer4/TextureRect
 @onready var user_answer_input = $Panel/VBoxContainer/MarginContainer3/UserAnswer
+@onready var time_remaining = $Panel/VBoxContainer/TitleMargin2/VBoxContainer/TimeRemaining 
+@onready var timer = $Timer
+var tick_player
+
 var player : CharacterBody2D
 var answers = {
 		"math1.png": "10",
@@ -20,12 +24,26 @@ func _ready() -> void:
 	if players.size() > 0:
 		player = players[0]
 	$Panel/VBoxContainer/MarginContainer3/UserAnswer.grab_focus()
+	tick_player = AudioManager.play_sfx("sfx/clock-ticking")
 
-func _on_button_pressed() -> void:
+func _process(delta: float) -> void:
+	var seconds = int(timer.get_time_left());
+	if seconds < 10:
+		time_remaining.add_theme_color_override("font_color", Color.RED)
+	time_remaining.text = "00:%02d" % seconds
+	
+func check_answer() -> void:
 	var correct_answer = answers[exercise_texture_rect.texture.resource_path.get_file()]
 	var user_answer = user_answer_input.text
 	if(user_answer != correct_answer):
 		player.take_damage(33)
 	get_parent().get_node("Boss1").process_mode = Node.PROCESS_MODE_PAUSABLE
 	get_parent().get_node("Player").process_mode = Node.PROCESS_MODE_PAUSABLE
+	tick_player.stop();
 	queue_free()
+	
+func _on_button_pressed() -> void:
+	check_answer()
+
+func _on_timer_timeout() -> void:
+	check_answer()
