6a38540475834e052f11f4ee77e201854e90cba1
Krzysztof <krzysztof@MSI.Bravo>
2026-01-20 12:39:23 +0100

    Reduces boss2 attack cooldown and increases damage to balance the boss fight.

--- scripts/enemies/boss_2.gd
+++ scripts/enemies/boss_2.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 signal health_changed(current_hp, max_hp)
 signal boss_defeated
 
-var health_points = 200
+var health_points = 300
 var max_health_points = health_points
 
-var attack_cooldown_time := 1.5 
-var special_attack_cooldown_time := 10.0
+var attack_cooldown_time := 0.5 
+var special_attack_cooldown_time := 8.0
 var current_cooldown: float = 0.0
 
 var direction := 1
@@ -71,6 +71,7 @@ func perform_special_attack():
 	
 	anim.play("special_attack")
 	electric_shock_player = AudioManager.play_sfx("sfx/electric-shock")
+	AudioManager.play_sfx("sfx/pioruny")
 	while anim.frame < 12:
 		if anim.animation != "special_attack" or player_in_range: 
 			$ElectricArc.visible = false
@@ -110,7 +111,7 @@ func perform_attack():
 			if attack_area.overlaps_body(player):
 				if player.has_method("take_damage"):
 					AudioManager.play_sfx("sfx/book_hit")
-					player.take_damage(1)
+					player.take_damage(10)
 					damage_dealt = true
 		await get_tree().process_frame
 	anim.play("idle")
